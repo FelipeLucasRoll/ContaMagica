5258e1f8adf7f0c7d3c676ffaf6ecc24279479da
FelipeLucasRoll <flp_roll@hotmail.com>
2020-04-19 14:12:17 -0300

    Adicionado projeto Maven

---
+++ ContaMagica/src/main/java/com/DesenvSoft/ContaMagica.java
@@ -0,0 +1,25 @@
+public class ContaMagica {
+    
+    public static final int SILVER = 0;
+    public static final int GOLD = 1;
+    public static final int PLATINUM = 2;
+    public double saldo = 0;
+    public int status;
+    
+    public double getSaldo(){
+        return saldo;
+    }
+	
+     public int getStatus(){
+         return status;
+          
+     }
+	
+     public void deposito(int valor) throws INVALID_OPER_EXCEPTION{
+
+     }
+
+     public  void retirada(int valor) throws INVALID_OPER_EXCEPTION{
+
+     }
+}
---
+++ ContaMagica/src/test/java/com/DesenvSoft/ContaMagicaTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Assertions;​
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ContaMagicaTest{
+
+    public ContaMagica cm;
+
+    @BeforeEach
+    public void setup(){
+        cm = new ContaMagica();
+    }
+}
