5e4c54fa92cddad68c49ca819debf8e866961c79
FelipeLucasRoll <57493030+FelipeLucasRoll@users.noreply.github.com>
2020-04-25 14:49:48 -0300

    Implementacao de casos de teste

--- ContaMagica/src/test/java/com/DesenvSoft/ContaMagicaTest.java
+++ ContaMagica/src/test/java/com/DesenvSoft/ContaMagicaTest.java
@@ -1,5 +1,6 @@
 package com.DesenvSoft;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +10,75 @@ public class ContaMagicaTest{
 
     @BeforeEach
     public void setup(){
-        cm = new ContaMagica();
+        this.cm = new ContaMagica();
     }
 
+
+    @Test
+    public void saldoNegativo(){
+        cm.deposito(10);
+        cm.retirada(1000);
+        double result = cm.getSaldo();
+      Assertions.assertEquals(0, result) ;
+    }
+
+    @Test
+    public void status01(){
+        cm.deposito(50000);
+        double result = cm.getStatus();
+      Assertions.assertEquals(1, result) ;
+    }
+
+    @Test
+    public void status12(){
+        cm.deposito(50000);
+        cm.deposito(150000);
+        double result = cm.getStatus();
+      Assertions.assertEquals(2, result) ;
+    }
+
+    @Test
+    public void statusValorizado(){
+        cm.deposito(50000);
+        cm.deposito(150000);
+        double result = cm.getSaldo();
+      Assertions.assertEquals(201500, result) ;
+    }
+
+    @Test
+    public void status21(){
+        cm.deposito(100000);
+        cm.deposito(100000);
+        cm.retirada(105000);
+        double result = cm.getStatus();
+      Assertions.assertEquals(1, result) ;
+    }
+
+    @Test
+    public void status21Exagero(){
+        cm.deposito(100000);
+        cm.deposito(100000);
+        cm.retirada(105000000);
+        double result = cm.getStatus();
+      Assertions.assertEquals(1, result) ;
+    }
+
+    @Test
+    public void status10Exagero(){
+        cm.deposito(100000);
+        cm.retirada(760000000);
+        System.out.println(cm.getSaldo());
+        double result = cm.getStatus();
+      Assertions.assertEquals(0, result) ;
+    }
+    
+
+    @Test
+    public void status10(){
+        cm.deposito(100000);
+        cm.retirada(76000);
+        double result = cm.getStatus();
+      Assertions.assertEquals(0, result) ;
+    }
 
 }
