fa38d6ac5591d06eed542f0ea7b890d33c9ef0b4
Douglas Pacheco <douglas.m.pacheco@outlook.com>
2020-04-26 17:43:18 -0300

    create InvalidOperException

---
+++ ContaMagica/src/main/java/com/DesenvSoft/InvalidOperException.java
@@ -0,0 +1,16 @@
+package com.DesenvSoft;
+
+public class InvalidOperException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     */
+    
+    public InvalidOperException(String message) {
+        super(message);
+    }
+}
