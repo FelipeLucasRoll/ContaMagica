65b9e77412ef245e3b34e0608a7f7e7f8a0b2daf
Douglas Pacheco <douglas.m.pacheco@outlook.com>
2020-04-26 18:17:40 -0300

    Throw InvalidOperException when "saldo" would be negative during retirada() and abort. Also reorderer conditionals to avoid unnecessary checks

--- ContaMagica/src/main/java/com/DesenvSoft/ContaMagica.java
+++ ContaMagica/src/main/java/com/DesenvSoft/ContaMagica.java
@@ -60,42 +60,24 @@ public void retirada(int valor) throws InvalidOperException{
     if(valor <=0 ){
         throw new InvalidOperException("Esta operação requer um valor maior ou igual a zero");
     }
-
-        if(this.getStatus() == 0){
-            this.saldo = this.saldo - valor;
-
-            if(this.saldo < 0){
-                this.saldo = 0;        
-            }
-
+        double saldoInicial = this.saldo;
+        this.saldo = this.saldo - valor;
+        if(this.saldo < 0){
+            this.saldo = saldoInicial;
+            throw new InvalidOperException("Esta operação resultaria em saldo inferior a zero e foi abortada. Nenhuma modificação foi realizada");
         }
 
-        else if(this.getStatus() == 1){
-            this.saldo = this.saldo - valor;
-
+        if(this.getStatus() == 1){
             if(this.saldo < 25000 ){
                 this.status = 0;
-
-
-            if(this.saldo < 0){
-                this.saldo = 0;
             }
         }
-    }
 
-       else if(this.getStatus() == 2){
-            this.saldo = this.saldo - valor;
-
+        else if(this.getStatus() == 2){
             if(this.saldo < 100000 ){
                 this.status = 1;
-
-            if(this.saldo < 0){
-                this.saldo = 0;
             }
+         }
+}
 
-        }
-
-
-     }
-} 
 }
